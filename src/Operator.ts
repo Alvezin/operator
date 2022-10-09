1596ccbfefa3fed2d3b0cd2af42399f3b9a6cafe
Gabriel Alves <99101659+Alvezin@users.noreply.github.com>
2022-10-09 17:09:08 -0300

    Criação métodos de divisão e subtração monetários

--- src/Operator.ts
+++ src/Operator.ts
@@ -32,11 +32,23 @@ export class Operator extends MoneyOperators {
         return acc
     }
 
+    public CurrencySubtract([...numbers]:(number | string)[], options:currencyOpt) {
+        const list = this.currencyToNumber(...numbers)
+        const finalSum = this.subtract(list[0], ...list.splice(1))
+        return this.NumberToCurrency(finalSum, options)
+    }
+
     public  multiply(...numbers:number[]){
         return numbers.reduce((num:number, acc:number)=>{
             return num * acc
         },1)
     }
+
+    public CurrencyMultiply([...numbers]:(number | string)[], options:currencyOpt) {
+        const finalSum = this.multiply(...this.currencyToNumber(...numbers))
+        return this.NumberToCurrency(finalSum, options)
+    }
+
 
     public divide(num1:number, ...numbers:number[]){
         let acc = num1
--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,7 @@ const operator = new Operator()
 
 let arr = [10,5,-5,2,1,5,4]
 
-console.log(operator.subtract(10))
-console.log(`Multiplicação: ${ operator.multiply(1,5,-10) }`)
-console.log(`Divisão: ${ operator.divide(1,100000000) }`)
-console.log(`Porcentagem: ${ operator.getPercentOf(25, 11200) }`)
-console.log(`Porcentagem: ${ operator.XIsHowPercentOf(2, 9) }`)
-
-console.log(`fatorial: ${ operator.getFactorial(10) }`)
-console.log(`soma dinheiro: ${ operator.CurrencySum(['1,40',3.54],{
+console.log(`mutlipicacao dinheiro: ${ operator.CurrencyMultiply([1.50,3],{
     countryCurrency: 'pt-BR',
     currency: 'BRL'
 }) }`)
