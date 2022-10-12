b9b7b94ca5bdb2e25dd77b7756364f8688332c3b
Gabriel Alves <99101659+Alvezin@users.noreply.github.com>
2022-10-12 14:20:17 -0300

    Criação metodo divisão monetarias, atualização do readme

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,3 @@ import { Operator } from "./Operator";
 
 const operator = new Operator()
 
-let arr = [10,5,-5,2,1,5,4]
-
-console.log(`mutlipicacao dinheiro: ${ operator.CurrencyMultiply([1.50,3],{
-    countryCurrency: 'pt-BR',
-    currency: 'BRL'
-}) }`)
